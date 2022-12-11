7bddb7b69f042614c96d20f22512c6d71867c955
Camarederic <camarederic@mail.ru>
2022-12-11 18:41:24 +0300

    Create a ShoppingViewModel

--- app/src/main/java/com/example/shoppinglistexample/ui/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistexample/ui/shoppinglist/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.shoppinglistexample
+package com.example.shoppinglistexample.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.example.shoppinglistexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProviders.of(this)[ShoppingViewModel::class.java]
     }
 }
