244ce6f055045b7446d5d4a0f43c72d866e1db11
Camarederic <camarederic@mail.ru>
2022-12-12 21:28:30 +0300

    Implement RecyclerView in MainActivity

--- app/src/main/java/com/example/shoppinglistexample/ui/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistexample/ui/shoppinglist/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.shoppinglistexample.ui.shoppinglist
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglistexample.R
 import com.example.shoppinglistexample.data.database.ShoppingDatabase
 import com.example.shoppinglistexample.data.repository.ShoppingRepository
+import com.example.shoppinglistexample.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +24,17 @@ class MainActivity : AppCompatActivity() {
         val factory = ShoppingViewModelFactory(repository)
 
         val viewModel = ViewModelProviders.of(this, factory)[ShoppingViewModel::class.java]
+
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+
     }
 }
